97eeb8f3734edbf473bf1517fc974ab197dc8bcd
Joseph Nudell <joenudell@gmail.com>
2015-08-19 11:27:40 -0700

    update tests

--- test/sanity/kmeans/basic.js
+++ test/sanity/kmeans/basic.js
@@ -1,5 +1,6 @@
-var clusterfck = require("../../../lib/clusterfck"),
-    equality = require("equality");
+var clusterfck = require("../../../lib/clusterfck");
+var equality = require("equality");
+var _ = require('underscore');
 
 var data = [
    [1, 1, 1],
@@ -11,13 +12,27 @@ var data = [
    [200, 200, 200]
 ];
 
+var objData = data.map(function(point) {
+   return {
+      a: point[0],
+      b: point[1],
+      c: point[2]
+   };
+});
+
+function withClusterKey(clusterIdx, row) {
+   return _.extend({}, row, {
+      '$__cluster': clusterIdx
+   });
+}
+
 var iterations = 20;
 
 exports.testTwoClusters = function(test) {
    for (var i = 0; i < iterations; i++) {
-      var clusters = clusterfck.kmeans(data, 2);
+      var clusters = clusterfck.kmeans(data, { k: 2 });
       test.ok(equality.members(clusters, [
-         [ 
+         [
             [1, 1, 1],
             [2, 2, 2],
             [3, 3, 3],
@@ -30,14 +45,14 @@ exports.testTwoClusters = function(test) {
          ]
       ]), "clustered into correct two clusters");
    }
-   test.done(); 
+   test.done();
 }
 
 exports.testThreeClusters = function(test) {
    for (var i = 0; i < iterations; i++) {
-      var clusters = clusterfck.kmeans(data, 3);
+      var clusters = clusterfck.kmeans(data, { k: 3 });
       test.ok(equality.members(clusters, [
-         [ 
+         [
             [1, 1, 1],
             [2, 2, 2],
             [3, 3, 3],
@@ -52,6 +67,41 @@ exports.testThreeClusters = function(test) {
          ]
       ]), "clustered into correct three clusters");
    }
-   test.done(); 
+   test.done();
 }
 
+exports.testClusterKeys = function(test) {
+   for (var i = 0; i < iterations; i++) {
+      var clusters = clusterfck.kmeans(objData, {
+         k: 3,
+         keys: ['a', 'b', 'c']
+      });
+
+      var expected = [
+         objData.slice(0, 5),
+         objData.slice(5, 6),
+         objData.slice(6, 7)
+      ];
+
+      clusters = clusters.sort(function(X, Y) {
+         return X[0].a < Y[0].a ? -1 : 1;
+      });
+
+      test.ok(
+         _.every(clusters, function(c, i) {
+            var e = expected[i];
+            var clusterId = c[0] && c[0]['$__cluster'];
+            return _.every(c, function(d, j) {
+               return (
+                  d.a === e[j].a &&
+                  d.b === e[j].b &&
+                  d.c === e[j].c &&
+                  d['$__cluster'] === clusterId
+               );
+            });
+         }),
+         "clustered into correct three clusters"
+      );
+   }
+   test.done();
+};
--- test/sanity/kmeans/distance.js
+++ test/sanity/kmeans/distance.js
@@ -1,5 +1,7 @@
-var clusterfck = require("../../../lib/clusterfck"),
-    equality = require("equality");
+var clusterfck = require("../../../lib/clusterfck");
+var distance = require("../../../lib/distance");
+var equality = require("equality");
+
 
 var data = [
    [1, 1, 1],
@@ -11,22 +13,30 @@ var data = [
 
 var iterations = 20;
 
-var distance = function(a, b) {
+var distFunc = function(a, b) {
    return Math.abs(a[0] - b[0]);
 }
 
 exports.testDistFunc = function(test) {
    for (var i = 0; i < iterations; i++) {
-      var clusters = clusterfck.kmeans(data, 2, distance);
-
-      console.log(clusters);
+      var clusters = clusterfck.kmeans(data, { k: 2, distance: distFunc });
 
       test.ok(equality.members(clusters,[ [[ 1, 1, 1 ], [ 5, 1, 1 ], [ 1, 5, 5 ] ,
               [ 4, 2, 2 ]], [[ 5, 5, 10000 ]] ]
       ), "clustered into correct two clusters");
    }
    test.done();
-}
+};
+
+exports.testEuclidean = function(test) {
+  var val = distance.euclidean(
+    { a: 1, b: 1 },
+    { a: 2, b: 2 },
+    ['a', 'b']
+  );
+  test.ok(val === Math.sqrt(2));
+  test.done();
+};
 
 // [ [ [ 1, 1, 1 ], [ 5, 1, 1 ], [ 1, 5, 5 ], [ 4, 2, 2 ] ],
 //  [ [ 5, 5, 10000 ] ] ]
